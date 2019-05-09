732e1e747235a8dfbf79da9ca515ae60b8c087ed
gcmiao <gc@roastew.com>
2019-05-09 23:53:04 +0200

    add swap chain

--- vk_triangle.jl
+++ vk_triangle.jl
@@ -10,6 +10,10 @@ logicalDevice = Ref{vk.VkDevice}()
 graphicsQueue = Ref{vk.VkQueue}()
 surface = vk.VkSurfaceKHR
 presentQueue = Ref{vk.VkQueue}()
+requiredExtensions = [vk.VK_KHR_SWAPCHAIN_EXTENSION_NAME]
+swapChain = Ref{vk.VkSwapchainKHR}()
+swapChainImageFormat = vk.VkFormat
+swapChainExtent = vk.VkExtent2D
 
 #################### 1.Create instance ####################
 function getAppInfo()
@@ -53,6 +57,21 @@ end
 
 #################### 2.Using validation layers ####################
 #################### 3.Pick Physical device ####################
+function checkDeviceExtensionSupport(device)
+    extensionCount = Ref{UInt32}(0)
+    vk.vkEnumerateDeviceExtensionProperties(device, C_NULL, extensionCount, C_NULL)
+
+    availableExtensions = Vector{vk.VkExtensionProperties}(undef, extensionCount[])
+    vk.vkEnumerateDeviceExtensionProperties(device, C_NULL, extensionCount, availableExtensions)
+
+    deviceExtensions = Set(requiredExtensions)
+    supportAll = true
+    for extension in availableExtensions
+        delete!(deviceExtensions, String(filter(x->x!=0, UInt8[extension.extensionName...])))
+    end
+    return length(deviceExtensions) == 0
+end
+
 function isDeviceSuitable(device::vk.VkPhysicalDevice)
     deviceProperties = Ref{vk.VkPhysicalDeviceProperties}()
     deviceFeatures = Ref{vk.VkPhysicalDeviceFeatures}()
@@ -61,6 +80,19 @@ function isDeviceSuitable(device::vk.VkPhysicalDevice)
     if (deviceFeatures[].geometryShader
         #&&deviceProperties[].deviceType == vk.VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU
         ) == vk.VK_FALSE
+        return false
+    end
+
+    extensionsSupported = checkDeviceExtensionSupport(device)
+    println(extensionsSupported)
+    if !extensionsSupported
+        return false
+    end
+
+    swapChainAdequate = false
+    swapChainSupport = querySwapChainSupport(device)
+    swapChainAdequate = length(swapChainSupport.formats) > 0 && length(swapChainSupport.presentModes) > 0
+    if !swapChainAdequate
         return false
     end
 
@@ -171,8 +203,8 @@ function createLogicalDevice()
         pointer(queueCreateInfos),
         0, #enabledLayerCount, VilidationLayers is disabled
         C_NULL, #ppEnabledlayerNames
-        0, #enabledExtensionCount
-        C_NULL, #ppEnabledExtensionNames
+        length(requiredExtensions), #enabledExtensionCount
+        strings2pp(requiredExtensions), #ppEnabledExtensionNames
         Base.unsafe_convert(Ptr{vk.VkPhysicalDeviceFeatures}, deviceFeatures)
     ))
     println("ready")
@@ -183,7 +215,8 @@ function createLogicalDevice()
         println("failed to create logical device!")
     end
 
-    vk.vkGetDeviceQueue(logicalDevice[], indices.presentFamily, 0, graphicsQueue)
+    vk.vkGetDeviceQueue(logicalDevice[], indices.graphicsFamily, 0, graphicsQueue)
+    vk.vkGetDeviceQueue(logicalDevice[], indices.presentFamily, 0, presentQueue)
 end
 
 #################### 6.Create window surface ####################
@@ -191,7 +224,126 @@ function createSurface()
     global surface = GLFW.CreateWindowSurface(instance[], window)
 end
 
+#################### 7.Swap chain ####################
+mutable struct SwapChainSupportDetails
+    capabilities::Ref{vk.VkSurfaceCapabilitiesKHR}
+    formats::Vector{vk.VkSurfaceFormatKHR}
+    presentModes::Vector{vk.VkPresentModeKHR}
 
+    SwapChainSupportDetails() = new(Ref{vk.VkSurfaceCapabilitiesKHR}(),
+                                    Vector{vk.VkSurfaceFormatKHR}(undef, 1),
+                                    Vector{vk.VkPresentModeKHR}(undef, 1))
+end
+
+function querySwapChainSupport(device::vk.VkPhysicalDevice)
+    details = SwapChainSupportDetails()
+    vk.vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, details.capabilities)
+    formatCount = Ref{UInt32}()
+    vk.vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, formatCount, C_NULL)
+    if (formatCount != 0)
+        resize!(details.formats, formatCount[])
+        vk.vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, formatCount, details.formats)
+    end
+
+    presentModeCount = Ref{UInt32}()
+    vk.vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, presentModeCount, C_NULL)
+    if (presentModeCount != 0)
+        resize!(details.presentModes, presentModeCount[])
+        vk.vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, presentModeCount, details.presentModes)
+    end
+    return details
+end
+
+function chooseSwapSurfaceFormat(availableFormats::Vector{vk.VkSurfaceFormatKHR})
+    if (length(availableFormats) == 1 && availableFormats[0].format == vk.VK_FORMAT_UNDEFINED)
+        return [vk.VK_FORMAT_B8G8R8A8_UNORM, vk.VK_COLOR_SPACE_SRGB_NONLINEAR_KHR]
+    end
+
+    for availableFormat in availableFormats
+        if (availableFormat.format == vk.VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace == vk.VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+            return availableFormat
+        end
+    end
+    return availableFormats[0]
+end
+
+function chooseSwapPresentMode(availablePresentModes::Vector{vk.VkPresentModeKHR})
+    bestMode = vk.VK_PRESENT_MODE_FIFO_KHR
+    for availablePresentMode in availablePresentModes
+        if availablePresentMode == vk.VK_PRESENT_MODE_MAILBOX_KHR
+            return availablePresentMode
+        elseif availablePresentMode == vk.VK_PRESENT_MODE_IMMEDIATE_KHR
+            bestMode = availablePresentMode
+        end
+    end
+    return bestMode
+end
+
+function chooseSwapExtent(capabilities::vk.VkSurfaceCapabilitiesKHR)
+    if capabilities.currentExtent.width != typemax(UInt32)
+        return capabilities.currentExtent
+    else
+        actualExtent = vk.VkExtent2D(WIDTH, HEIGHT)
+        actualExtent.width = max(capabilities.minImageExtent.width, min(capabilities.maxImageExtent.width, actualExtent.width))
+        actualExtent.height = max(capabilities.minImageExtent.height, min(capabilities.maxImageExtent.height, actualExtent.height))
+        return actualExtent
+    end
+end
+
+function createSwapChain()
+    swapChainSupport = querySwapChainSupport(physicalDevice)
+    surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats)
+    presentMode = chooseSwapPresentMode(swapChainSupport.presentModes)
+    global swapChainExtent = chooseSwapExtent(swapChainSupport.capabilities[])
+
+    imageCount = swapChainSupport.capabilities[].minImageCount + 1
+    if (swapChainSupport.capabilities[].maxImageCount > 0 && imageCount > swapChainSupport.capabilities[].maxImageCount)
+        imageCount = swapChainSupport.capabilities[].maxImageCount
+    end
+    global swapChainImageFormat = surfaceFormat.format
+
+    indices = findQueueFamilies(physicalDevice)
+
+    imageSharingMode = vk.VK_SHARING_MODE_EXCLUSIVE
+    queueFamilyIndexCount = 0 # Optional
+    queueFamilyIndices = C_NULL # Optional
+    if (indices.graphicsFamily != indices.presentFamily)
+        imageSharingMode = vk.VK_SHARING_MODE_CONCURRENT
+        queueFamilyIndexCount = 2
+        queueFamilyIndices = [indices.graphicsFamily, indices.presentFamily]
+    end
+
+    createInfo = Ref(vk.VkSwapchainCreateInfoKHR(
+        vk.VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR, #sType
+        C_NULL, #pNext
+        vk.VkFlags(0), #flags
+        surface, #surface
+        imageCount, #minImageCount
+        swapChainImageFormat, #imageFormat
+        surfaceFormat.colorSpace, #imageColorSpace
+        swapChainExtent, #imageExtent
+        1, #imageArrayLayers
+        vk.VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, #imageUsage
+        imageSharingMode, #imageSharingMode::VkSharingMode
+        queueFamilyIndexCount, #queueFamilyIndexCount::UInt32
+        queueFamilyIndices, #pQueueFamilyIndices::Ptr{UInt32}
+        swapChainSupport.capabilities[].currentTransform, #preTransform::VkSurfaceTransformFlagBitsKHR
+        vk.VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR, #compositeAlpha::VkCompositeAlphaFlagBitsKHR
+        presentMode, #presentMode::VkPresentModeKHR
+        vk.VK_TRUE, #clipped::VkBool32
+        vk.VK_NULL_HANDLE #oldSwapchain::VkSwapchainKHR
+    ))
+
+    if (vk.vkCreateSwapchainKHR(logicalDevice[], createInfo, C_NULL, swapChain) != vk.VK_SUCCESS)
+        println("failed to create swap chain!")
+    end
+
+    swapChainImages = Vector{vk.VkImage}()
+    imageCount = Ref{UInt32}()
+    vk.vkGetSwapchainImagesKHR(logicalDevice[], swapChain[], imageCount, C_NULL)
+    resize!(swapChainImages, imageCount[])
+    vk.vkGetSwapchainImagesKHR(logicalDevice[], swapChain[], imageCount, swapChainImages)
+end
 
 function vkDestoryInstanceCallback()
     println("callback")
@@ -213,6 +365,7 @@ function initVulkan()
     createSurface()
     pickPhysicalDevice()
     createLogicalDevice()
+    createSwapChain()
 end
 
 function initWindow()
@@ -234,6 +387,7 @@ function mainLoop()
 end
 
 function cleanup()
+    vk.vkDestroySwapchainKHR(logicalDevice[], swapChain[], C_NULL)
     vk.vkDestroyDevice(logicalDevice[], C_NULL)
     vk.vkDestroySurfaceKHR(instance[], surface, C_NULL)
     vk.vkDestroyInstance(instance[], C_NULL)
